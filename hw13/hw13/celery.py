5d137022e148b765d5f1d6d43fb36559ae569734
Denis <denis.litvinenko@yahoo.com>
2021-09-21 15:10:42 +0300

    ...

--- hw13/hw13/celery.py
+++ hw13/hw13/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw13.settings')
 
@@ -10,11 +11,11 @@ app = Celery('hw13')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-# app.conf.beat_schedule = {
+app.conf.beat_schedule = {
 #     'parse': {
 #         'task': 'tasks.parse',
 #         'schedule': crontab(minute=0, second=0, hour="1-23/2"),
-#         'args':
+#         'args': (1, 0, 4)
 #     }
-# }
+}
 
