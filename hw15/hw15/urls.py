e6c341ced99e8b11ef7f81e5506fe4363269ddea
DenisL87 <denis.lytvynenko@gmail.com>
2021-09-29 11:02:33 +0300

    ...

--- hw15/hw15/urls.py
+++ hw15/hw15/urls.py
@@ -1,4 +1,4 @@
-"""hw14 URL Configuration
+"""hw15 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from new_app import views
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
