e77cb9df36ccb6bd0d617f38bad3aba4980b88c2
DenisL87 <denis.lytvynenko@gmail.com>
2021-11-09 18:11:14 +0200

    ...

--- chi_test_task/chi_test_task/settings.py
+++ chi_test_task/chi_test_task/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x*3ya_7)g4u*ti_4%3lt4nue0!=x#rk0#d5kdo8)7ao++3+3l6'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-x*3ya_7)g4u*ti_4%3lt4nue0!=x#rk0#d5kdo8)7ao++3+3l6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
