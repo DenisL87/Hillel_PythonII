cf1d432d5533534e58488d9a4427d2f3d4832c5d
DenisL87 <denis.lytvynenko@gmail.com>
2021-11-12 17:37:05 +0200

    ...

--- chi_test_task/address_book/api/api_views.py
+++ chi_test_task/address_book/api/api_views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
 
 from .serializers import PersonSerializer
 from ..models import Person
@@ -17,13 +19,28 @@ class NewEntryAPIView(CreateAPIView):
 class SearchAPIView(RetrieveAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    lookup_field = 'id'
 
 
 class EditAPIView(UpdateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    http_method_names = ['get', 'head', 'post']
+
+    def get(self, request, *args, **kwargs):
+        serializer = PersonSerializer(Person.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        self.http_method_names.append("GET")
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DeleteAPIView(DestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    lookup_field = 'id'
--- chi_test_task/address_book/api/urls.py
+++ chi_test_task/address_book/api/urls.py
@@ -5,7 +5,8 @@ from .api_views import PersonListAPIView, NewEntryAPIView, EditAPIView, DeleteAP
 urlpatterns = [
     path('address_book/', PersonListAPIView.as_view(), name='address_book'),
     path('new_entry/', NewEntryAPIView.as_view(), name='new_entry'),
-    path('edit_entry/<int:id>/', EditAPIView.as_view(), name='edit_entry'),
+    path('search_entry', SearchAPIView.as_view(), name='search_entry'),
+    path('edit_entry/', EditAPIView.as_view(), name='edit_entry'),
     path('search_entry/<int:id>/', SearchAPIView.as_view(), name='search_entry'),
     path('delete_entry/<int:id>/', DeleteAPIView.as_view(), name='delete_entry'),
 ]
