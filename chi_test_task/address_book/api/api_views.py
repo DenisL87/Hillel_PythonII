ed1a24530f845400a22ffdd165e3d34ac022fc85
DenisL87 <denis.lytvynenko@gmail.com>
2021-11-16 01:27:10 +0200

    ...

--- chi_test_task/address_book/api/api_views.py
+++ chi_test_task/address_book/api/api_views.py
@@ -1,10 +1,8 @@
-from rest_framework import status
 from rest_framework.generics import (ListAPIView,
                                      RetrieveAPIView,
                                      CreateAPIView,
                                      UpdateAPIView,
                                      DestroyAPIView)
-from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -12,12 +10,15 @@ from .serializers import PersonSerializer
 from ..models import Person
 
 
-class PersonListAPIView(ListAPIView):
-    serializer_class = PersonSerializer
+class DeleteAPIView(RetrieveAPIView, DestroyAPIView):
     queryset = Person.objects.all()
-    filter_backends = (DjangoFilterBackend, SearchFilter)
-    filter_fields = ['id', 'first_name', 'last_name', 'phone', 'address', 'url']
-    search_fields = ['id', 'first_name', 'last_name', 'phone', 'address', 'url']
+    serializer_class = PersonSerializer
+    lookup_field = 'id'
+
+
+class EditAPIView(RetrieveAPIView, UpdateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
 
 
 class NewEntryAPIView(CreateAPIView):
@@ -25,33 +26,9 @@ class NewEntryAPIView(CreateAPIView):
     serializer_class = PersonSerializer
 
 
-# class SearchAPIView(ListAPIView):
-#     queryset = Person.objects.all()
-#     serializer_class = PersonSerializer
-#     filter_backends = (DjangoFilterBackend, SearchFilter)
-#     filter_fields = ['id', 'first_name', 'last_name', 'phone', 'address', 'url']
-#     search_fields = ['id', 'first_name', 'last_name', 'phone', 'address', 'url']
-
-
-class EditAPIView(UpdateAPIView):
-    queryset = Person.objects.all()
+class PersonListAPIView(ListAPIView):
     serializer_class = PersonSerializer
-    http_method_names = ['get', 'head', 'post']
-
-    def get(self, request, *args, **kwargs):
-        serializer = PersonSerializer(Person.objects.all(), many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        self.http_method_names.append("GET")
-        serializer = PersonSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class DeleteAPIView(RetrieveAPIView, DestroyAPIView):
     queryset = Person.objects.all()
-    serializer_class = PersonSerializer
-    lookup_field = 'id'
+    filter_backends = (SearchFilter, DjangoFilterBackend)
+    search_fields = ['id', 'first_name', 'last_name', 'phone', 'address', 'url']
+    filter_fields = ['id', 'first_name', 'last_name', 'phone', 'address', 'url']
--- chi_test_task/address_book/api/urls.py
+++ chi_test_task/address_book/api/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 
 from .api_views import (PersonListAPIView,
                         NewEntryAPIView,
-                        EditAPIView,
-                        DeleteAPIView)
+                        DeleteAPIView,
+                        EditAPIView)
 
 urlpatterns = [
     path('address_book/', PersonListAPIView.as_view(), name='address_book'),
+    path('delete/<int:id>/', DeleteAPIView.as_view(), name='delete_entry'),
+    path('edit/<int:pk>/', EditAPIView.as_view(), name='edit_entry'),
     path('new_entry/', NewEntryAPIView.as_view(), name='new_entry'),
-    # path('search_entry', SearchAPIView.as_view(), name='search_entry'),
-    path('edit_entry/', EditAPIView.as_view(), name='edit_entry'),
-    path('delete_entry/<int:id>/', DeleteAPIView.as_view(), name='delete_entry'),
 ]
